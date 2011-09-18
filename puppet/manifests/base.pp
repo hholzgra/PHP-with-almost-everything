365d0a7e4f265308af69d07b7b3901898a9f3d14
Hartmut Holzgraefe <hartmut@php.net>
2011-09-18 23:40:56 +0200

    install development packages to allow for as many php extenions to be built as possible

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -48,3 +48,121 @@ file { "/home/vagrant/.bash_aliases":
   content => "export PHP_AUTOCONF=autoconf2.59",
   owner   => "vagrant",
 }  
+
+### additional extensions
+
+# bz2
+package { ["libbz2-dev"]:
+  ensure => present,
+}
+
+# curl
+package { ["libcurl4-openssl-dev"]:
+  ensure => present,
+}
+
+# dba
+package { ["libgdbm-dev","libqdbm-dev","libcdb-dev"]:
+  ensure => present,
+}
+
+# enchant
+package { ["libenchant-dev"]:
+  ensure => present,
+}
+
+# gd
+package { ["libgd2-xpm-dev","libfontconfig1-dev","libfreetype6-dev"]:
+  ensure => present,
+}
+
+# gmp
+package { ["libgmp3-dev"]:
+  ensure => present,
+}
+
+# imap
+package { ["libc-client2007e-dev"]:
+  ensure => present,
+}
+
+# interbase / firebird
+package { ["firebird2.5-dev"]:
+  ensure => present,
+}
+
+# ldap
+package { ["libldap2-dev"]:
+  ensure => present,
+}
+
+# mbstring ?
+package { ["libicu-dev"]:
+  ensure => present,
+}
+
+# mcrypt 
+package { ["libmcrypt-dev"]:
+  ensure => present,
+}
+
+# MS SQL
+package { ["freetds-dev"]:
+  ensure => present,
+}
+
+# mysql
+package { ["libmysqlclient-dev"]:
+  ensure => present,
+}
+
+# odbc
+package { ["unixodbc-dev"]:
+  ensure => present,
+}
+
+# openssl
+package { ["libssl-dev"]:
+  ensure => present,
+} 
+
+# postgresql
+package { ["libpq-dev"]:
+  ensure => present,
+}
+
+# pspell
+package { ["libpspell-dev"]:
+  ensure => present,
+}
+
+# readline 
+package { ["libreadline-dev"]:
+  ensure => present,
+}
+
+# recode
+package { ["librecode-dev"]:
+  ensure => present,
+}
+
+# snmp 
+package { ["libsnmp-dev"]:
+  ensure => present,
+}
+
+# tidy
+package { ["libtidy-dev"]:
+  ensure => present,
+}
+
+# wddx
+package { ["libexpat1-dev"]:
+  ensure => present,
+}
+
+# xslt
+package { ["libxslt1-dev"]:
+  ensure => present,
+}
+
